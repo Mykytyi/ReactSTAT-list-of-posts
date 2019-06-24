23b06013d811bf3033d48102f03269d520fa45ba
Nikita <cherevaty.nik@gmail.com>
2019-06-24 23:11:00 +0300

    Updates

---
+++ precache-manifest.8cf38272545de4c3baf662c7bbae4140.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3184ce3f53a1e84f7ea41cb5ed140258",
+    "url": "/ReactSTAT-list-of-posts/index.html"
+  },
+  {
+    "revision": "96e7f4954de11dbc1146",
+    "url": "/ReactSTAT-list-of-posts/static/css/main.6e52415a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/ReactSTAT-list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "96e7f4954de11dbc1146",
+    "url": "/ReactSTAT-list-of-posts/static/js/main.ccedb97f.chunk.js"
+  },
+  {
+    "revision": "6dc41801dc2b1b38101e",
+    "url": "/ReactSTAT-list-of-posts/static/js/runtime~main.09bc2838.js"
+  }
+]);
